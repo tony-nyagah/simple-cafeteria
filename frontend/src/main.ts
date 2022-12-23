5ae21832cafb70d384488aa8e9f385b4485e3b91
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-12-23 06:34:01 +0300

    initial vue and daisy ui setup

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import "./index.css";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
